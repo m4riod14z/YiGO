87e9b84705f029b2f929c981f2c0e30079f30e9b
Juan Pablo Terán <134824274+teran1416@users.noreply.github.com>
2025-05-24 17:05:44 -0500

    Add files via upload

--- app/src/main/java/com/example/yigo/view/RegistroFundacion.kt
+++ app/src/main/java/com/example/yigo/view/RegistroFundacion.kt
@@ -1,5 +1,6 @@
 package com.example.yigo.view
 
+import TerminosDialogFragment
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
--- app/src/main/java/com/example/yigo/view/RegistroUsuario.kt
+++ app/src/main/java/com/example/yigo/view/RegistroUsuario.kt
@@ -1,5 +1,6 @@
 package com.example.yigo.view
 
+import TerminosDialogFragment
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
